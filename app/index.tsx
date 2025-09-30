4c7462bc9f25bf5b6a6fabc671eba3a99cae11c6
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-09-30 09:11:48 -0300

    feat: created the initial page

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,30 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import Background from "@/components/background";
+import { router } from "expo-router";
+import { Image, Pressable, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500">Welcome to the App!</Text>
-      <View>
-        <Link href="/signin">Sign In</Link>
-        <Link href="/signup">Sign Up</Link>
+    <Background>
+      <View className="flex-1 items-center justify-center">
+        <Image
+          source={require("../assets/Logo.png")}
+          className="w-430 h-932 mb-4"
+        />
+        <View className="flex-col gap-6 py-5">
+          <Pressable
+            className="bg-[#8162FF] px-28 py-5 rounded-xl hover:bg-[#9378fe]"
+            onPress={() => router.push("/signin")}
+          >
+            <Text className="text-white">Sign In</Text>
+          </Pressable>
+          <Pressable
+            className="bg-[#8162FF] px-28 py-5 rounded-xl"
+            onPress={() => router.push("/signup")}
+          >
+            <Text className="text-white">Sign Up</Text>
+          </Pressable>
+        </View>
       </View>
-    </View>
+    </Background>
   );
 }
