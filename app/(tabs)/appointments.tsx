e343818f5b2f21889b139538707bd4174e1f73cf
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-10-03 10:35:31 -0300

    feat: created the appointments screen

---
+++ app/(tabs)/appointments.tsx
@@ -0,0 +1,13 @@
+import Background from "@/components/background";
+import Title from "@/components/title";
+import { View } from "react-native";
+
+export default function Appointments() {
+  return (
+    <Background>
+      <View className="flex-1 items-center justify-center">
+        <Title>Appointments Screen</Title>
+      </View>
+    </Background>
+  );
+}
