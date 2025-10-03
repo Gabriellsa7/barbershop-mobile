d0b2bea886069110a0e089625c3751fc76348ee6
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-10-03 10:35:22 -0300

    feat: added new routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,22 @@
 import { Tabs } from "expo-router";
-import { House } from "lucide-react-native";
+import { Calendar, House, User } from "lucide-react-native";
 
 export default function TabsLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#06b6d4", // cor ativa (cyan-500)
-        tabBarInactiveTintColor: "#9ca3af", // cor inativa (gray-400)
+        tabBarActiveTintColor: "#8162FF", // Active Color (purple-500)
+        tabBarInactiveTintColor: "#FFFFFF", // Inactiver Color (white)
+        tabBarStyle: {
+          display: "flex",
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 10,
+          backgroundColor: "#141518", // Background Color (gray-900)
+          borderTopColor: "#4b5563", // Border Color (gray-700)
+          borderTopWidth: 2,
+        },
       }}
     >
       <Tabs.Screen
@@ -15,6 +24,22 @@ export default function TabsLayout() {
         options={{
           title: "Home",
           tabBarIcon: ({ color, size }) => <House color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="appointments"
+        options={{
+          title: "Appointments",
+          tabBarIcon: ({ color, size }) => (
+            <Calendar color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => <User color={color} size={size} />,
         }}
       />
     </Tabs>
