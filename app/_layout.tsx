a4469a8ffaf09ea08bdd736a8a899de43cec16cf
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-10-03 10:35:08 -0300

    feat: added the DateProvider in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { AuthProvider } from "@/contexts/auth-context";
+import { DateProvider } from "@/contexts/date-context";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
@@ -7,11 +8,13 @@ export default function RootLayout() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <AuthProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" options={{ title: "Initial Page" }} />
-          <Stack.Screen name="signin" options={{ title: "Signin" }} />
-          <Stack.Screen name="signup" options={{ title: "Signup" }} />
-        </Stack>
+        <DateProvider>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" options={{ title: "Initial Page" }} />
+            <Stack.Screen name="signin" options={{ title: "Signin" }} />
+            <Stack.Screen name="signup" options={{ title: "Signup" }} />
+          </Stack>
+        </DateProvider>
       </AuthProvider>
     </SafeAreaView>
   );
