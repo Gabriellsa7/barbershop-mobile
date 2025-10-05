bdaf477931b70900cc1acf49a61303afd6d206c1
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2025-10-05 10:12:19 -0300

    feat: changed the ip

--- app/signup.tsx
+++ app/signup.tsx
@@ -22,7 +22,7 @@ export default function Signup() {
 
   const handleSignup = async () => {
     try {
-      const response = await fetch("http://10.1.73.233:3000/api/users", {
+      const response = await fetch("http:/192.168.0.7:3000/api/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
