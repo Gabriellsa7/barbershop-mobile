9386ff68c317987fc69eb65420facd3fe6a9e9a9
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-01-11 11:20:58 -0300

    feat: add scroll view

--- app/(tabs)/appointments.tsx
+++ app/(tabs)/appointments.tsx
@@ -2,7 +2,7 @@ import { useGetAppointmentByUser } from "@/api/use-get-appointmen-by-user";
 import AppointementsCard from "@/components/appointments-card";
 import Background from "@/components/background";
 import Title from "@/components/title";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Appointments() {
   const { data: appointmentsData, loading } = useGetAppointmentByUser();
@@ -26,35 +26,37 @@ export default function Appointments() {
   }
   return (
     <Background>
-      <View className="items-center justify-center gap-5 p-5">
-        <Title>Appointments</Title>
-        {appointmentsData.length === 0 ? (
-          <Text className="text-white mt-2">Nenhum agendamento</Text>
-        ) : (
-          appointmentsData.map((appointment) => {
-            const services = appointment.appointmentservice;
+      <ScrollView>
+        <View className="items-center justify-center gap-5 p-5">
+          <Title>Appointments</Title>
+          {appointmentsData.length === 0 ? (
+            <Text className="text-white mt-2">Nenhum agendamento</Text>
+          ) : (
+            appointmentsData.map((appointment) => {
+              const services = appointment.appointmentservice;
 
-            return (
-              <AppointementsCard
-                key={appointment.id}
-                name={appointment.barbershop.name}
-                avatarUrl={appointment.barbershop.image_url}
-                date={`${appointment.date.split("T")[0]}T${
-                  appointment.startTime
-                }:00`}
-                startTime={appointment.startTime}
-                service={
-                  services.length > 0
-                    ? services.map((s) => s.service.name).join(", ")
-                    : "Serviço não informado"
-                }
-                status={appointment.status}
-                className="w-full"
-              />
-            );
-          })
-        )}
-      </View>
+              return (
+                <AppointementsCard
+                  key={appointment.id}
+                  name={appointment.barbershop.name}
+                  avatarUrl={appointment.barbershop.image_url}
+                  date={`${appointment.date.split("T")[0]}T${
+                    appointment.startTime
+                  }:00`}
+                  startTime={appointment.startTime}
+                  service={
+                    services.length > 0
+                      ? services.map((s) => s.service.name).join(", ")
+                      : "Serviço não informado"
+                  }
+                  status={appointment.status}
+                  className="w-full"
+                />
+              );
+            })
+          )}
+        </View>
+      </ScrollView>
     </Background>
   );
 }
