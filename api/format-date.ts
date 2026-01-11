683e1fd68d05afab18b74bce53a2a7e2b324fba8
Gabriellsa7 <gabrielsantanasantos798@gmail.com>
2026-01-11 10:42:50 -0300

    feat: add verification to format date

--- api/format-date.ts
+++ api/format-date.ts
@@ -1,4 +1,9 @@
 export function formatDate(date: string) {
   const d = new Date(date);
+
+  if (isNaN(d.getTime())) {
+    return "Data inválida";
+  }
+
   return d.toISOString().split("T")[0];
 }
